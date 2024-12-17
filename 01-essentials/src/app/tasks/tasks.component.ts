1a3fa897d37a3b8713b77c0eb0662d545be8d2c4
Eszlari, Zoltan <Zoltan.Eszlari@knorr-bremse.com>
2024-12-17 19:39:42 +0100

    conditional rendering (new task dialog)

--- 01-essentials/src/app/tasks/tasks.component.ts
+++ 01-essentials/src/app/tasks/tasks.component.ts
@@ -1,20 +1,34 @@
 import { Component, Input } from '@angular/core';
 import { DUMMY_TASKS } from '../dummy-tasks';
+import { NewTaskComponent } from "./new-task/new-task.component";
 import { TaskComponent } from "./task/task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input ({ required: true }) userId!: string;
   @Input ({ required: true }) name!: string;
+  isAddingTask = false;
   tasks = DUMMY_TASKS;
 
   get selectedUserTasks() {
     return this.tasks.filter(task => task.userId === this.userId);
+  }
+
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
   }
 }
