cb11c370a6cb6865a1aa705f0ed6c66331339286
eszizoli <zoltan.eszlari@gmail.com>
2024-12-30 14:40:31 +0100

    using reusable component

--- 06-cmp-deep-dive/src/app/app.component.ts
+++ 06-cmp-deep-dive/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
 import { TicketsComponent } from './dashboard/tickets/tickets.component';
 import { TrafficComponent } from './dashboard/traffic/traffic.component';
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
@@ -12,8 +13,9 @@ import { TrafficComponent } from './dashboard/traffic/traffic.component';
     HeaderComponent,
     ServerStatusComponent,
     TicketsComponent,
-    TrafficComponent
-  ],
+    TrafficComponent,
+    DashboardItemComponent
+],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
